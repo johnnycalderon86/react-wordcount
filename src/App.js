e501142ebd1a25a60d1db6d36a811a358bfa7f26
Johnny <johnnycalderondeveloper@gmail.com>
2020-08-01 09:14:21 +0200

    adding wordcount and counter logic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
-import React from 'react';
+
+import React, {useState, useEffect} from 'react'
 
 import './App.css';
 
-function App() {
+
+export default function App() {
+
+  
+  const [text, setText] = useState("")
+  const [timer, setTimer] = useState(60)
+
+  function handleChange(event){
+    const {value}= event.target
+
+    setText(value)
+  }
+
+  function wordCount(words){
+   const wordsArr = text.trim().split(' ')
+   const filtered = wordsArr.filter(word => word !== "")
+   return filtered.length
+  }
+//   useEffect(() => {
+//     if(timer >0){
+      
+//       setTimeout(() => {
+  
+//         setTimer(prevtime=> prevtime -1)
+//       }, 1000)
+//     }
+// }, [timer])
+  
+
+useEffect(()=>{
+  if(timer > 0){
+
+    setTimeout(()=>{
+      setTimer(prevState => prevState -1)
+    }, 1000)
+  }
+},[timer])
   return (
     <div className="App">
-      <h1>WordCount</h1>
-      <textarea/>
-
-      <h4>Time remaining</h4>
-    </div>
-  );
+          <h1>WordCunt</h1>
+           <textarea onChange={handleChange} value={text}/>
+    
+          <h4>Time remaining:{timer}</h4>
+           <button onClick={()=>{
+              wordCount(text)
+           }}>Start</button>
+         <h1>WordCunt</h1>
+        </div>
+  )
 }
 
-export default App;
