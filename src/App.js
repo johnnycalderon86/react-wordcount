0ca7f0d08c1311beef717cc29861d95a6a5bbf37
Johnny <johnnycalderondeveloper@gmail.com>
2020-08-01 10:31:28 +0200

    usseffect to start the timer when start is pressed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ export default function App() {
 
   
   const [text, setText] = useState("")
-  const [timer, setTimer] = useState(60)
+  const [timer, setTimer] = useState(5)
+  const [start, setStart] = useState(false)
 
   function handleChange(event){
     const {value}= event.target
@@ -19,17 +20,22 @@ export default function App() {
   function wordCount(words){
    const wordsArr = text.trim().split(' ')
    const filtered = wordsArr.filter(word => word !== "")
-   console.log(filtered.length);
+   
+    
    return filtered.length
 
    
   }
   
+
 useEffect(()=>{
-  setTimeout(()=>{
-    setTimer(prevState => prevState-1)
-  }, 1000)
-},[timer])
+  if(start && timer > 0){
+
+    setTimeout(()=>{
+      setTimer(prevState => prevState-1)
+    }, 1000)
+  }
+},[timer,start])
   return (
     <div className="App">
           <h1>WordCount</h1>
@@ -37,7 +43,7 @@ useEffect(()=>{
     
           <h4>Time remaining:{timer}</h4>
            <button onClick={()=>{
-              wordCount(text)
+              setStart(true)
            }}>Start</button>
          <h1>WordCunt</h1>
         </div>
