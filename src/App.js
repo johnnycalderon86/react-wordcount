72d8e847c880e214b43a7f5acd3713edb8a311c4
Johnny <johnnycalderondeveloper@gmail.com>
2020-08-03 18:44:12 +0200

    adding useref to ficus on textarea when clicking start

--- src/App.js
+++ src/App.js
@@ -1,52 +1,69 @@
 
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import './App.css';
 
 
 export default function App() {
+  const STARTING_TIME = 5
 
-  
   const [text, setText] = useState("")
-  const [timer, setTimer] = useState(5)
+  const [timer, setTimer] = useState(STARTING_TIME)
   const [start, setStart] = useState(false)
+  const [finalCount, setFinalCount] = useState(0)
 
-  function handleChange(event){
-    const {value}= event.target
+  const textAreaRef = useRef(null) 
+
+  function handleChange(event) {
+    const { value } = event.target
 
     setText(value)
   }
 
-  function wordCount(words){
-   const wordsArr = text.trim().split(' ')
-   const filtered = wordsArr.filter(word => word !== "")
-   
-    
-   return filtered.length
-
-   
+  function wordCount(words) {
+    const wordsArr = text.trim().split(' ')
+    const filtered = wordsArr.filter(word => word !== "")
+    return filtered.length
   }
-  
 
-useEffect(()=>{
-  if(start && timer > 0){
+  function startGame() {
+    setStart(true)
+    setTimer(STARTING_TIME)
+    setText("")
 
-    setTimeout(()=>{
-      setTimer(prevState => prevState-1)
-    }, 1000)
-  }
-},[timer,start])
+    textAreaRef.current.focus()
+
+}
+
+function endGame() {
+  setStart(false)
+  setFinalCount(wordCount(text))
+}
+
+
+  useEffect(() => {
+    if (start && timer > 0) {
+
+      setTimeout(() => {
+        setTimer(prevState => prevState - 1)
+      }, 1000)
+    } else if (timer === 0) {
+      endGame()
+    }
+
+  }, [timer, start])
+
+
+
   return (
     <div className="App">
-          <h1>WordCount</h1>
-           <textarea onChange={handleChange} value={text}/>
-    
-          <h4>Time remaining:{timer}</h4>
-           <button onClick={()=>{
-              setStart(true)
-           }}>Start</button>
-         <h1>WordCunt</h1>
-        </div>
+      <h1>WordCount</h1>
+      <textarea ref={textAreaRef} onChange={handleChange} value={text} />
+
+      <h4>Time remaining:{timer}</h4>
+      <button onClick={startGame}>Start</button>
+      <h1>WordCunt {finalCount}</h1>
+    </div>
   )
 }
 
